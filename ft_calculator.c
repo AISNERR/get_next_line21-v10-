27290ddf845a90fc46ee7a21c7eabe96b5a52154
Shirley Galadriel <sgaladri@su-a2.msk.21-school.ru>
2021-11-24 03:20:02 +0300

    switch_function

---
+++ ft_calculator.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calculator.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgaladri <sgaladri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/24 03:19:03 by sgaladri          #+#    #+#             */
+/*   Updated: 2021/11/24 03:19:05 by sgaladri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Program to create a simple calculator
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double n1, n2;
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operator);
+    printf("Enter two operands: ");
+    scanf("%lf %lf",&n1, &n2);
+
+    switch(operator)
+    {
+        case '+':
+            printf("%.1lf + %.1lf = %.1lf",n1, n2, n1+n2);
+            break;
+
+        case '-':
+            printf("%.1lf - %.1lf = %.1lf",n1, n2, n1-n2);
+            break;
+
+        case '*':
+            printf("%.1lf * %.1lf = %.1lf",n1, n2, n1*n2);
+            break;
+
+        case '/':
+            printf("%.1lf / %.1lf = %.1lf",n1, n2, n1/n2);
+            break;
+
+        // operator doesn't match any case constant +, -, *, /
+        default:
+            printf("Error! operator is not correct");
+    }
+
+    return 0;
+}
